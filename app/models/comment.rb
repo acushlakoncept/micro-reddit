836c84f8128d3277898e3cafb02a46be192f4611
Uduak Essien <acushla4real@gmail.com>
2020-07-28 14:28:32 +0100

    Add validation to user, post, comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :body, presence: true
+  validates :user_id, presence: true
+  validates :post_id, presence: true
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :title, presence: true
+  validates :url, presence: true
+  validates :user_id, presence: true
 end
