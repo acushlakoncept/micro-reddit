dfbc7cb4153af231c7f46743b1039d8e5ca4aad4
Somoye123 <somoye.ayotunde@gmail.com>
2020-07-28 15:36:31 -0700

    modified comment and user models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,4 @@ class Comment < ApplicationRecord
   validates :body, presence: true
   validates :user_id, presence: true
   validates :post_id, presence: true
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
+  has_many :posts
+  has_many :comments
 
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
-    validates :password, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 
-    # validates_each :username, :email do |presence, uniqueness|
-    #     presence: true
-    #     uniqueness: true
-    # end
+  # validates_each :username, :email do |presence, uniqueness|
+  #     presence: true
+  #     uniqueness: true
+  # end
 end
